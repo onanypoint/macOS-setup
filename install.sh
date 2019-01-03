c7b62367115aa84c4534e0d31103b9ca6c9c953c
onanypoint <onanypoint@gmail.com>
2019-01-03 12:48:04 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -2,11 +2,32 @@
 
 echo "Setting up your Mac..."
 
+# Paremeters
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+        COMPUTER_NAME=${OPTARG};;
+  esac
+done
+
+# Set computer name
+# https://ilostmynotes.blogspot.com/2012/03/computername-vs-localhostname-vs.html
+if [[ -z ${COMPUTER_NAME} ]] ; then
+    echo "Computer name not set"
+else
+    echo "Computer name set to ${COMPUTER_NAME}"
+    sudo scutil --set ComputerName "${COMPUTER_NAME}"
+    sudo scutil --set HostName "${COMPUTER_NAME}"
+    sudo scutil --set LocalHostName "${COMPUTER_NAME}"
+
+fi
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+echo "Installing stuff via Homebrew"
 # Update Homebrew recipes
 brew update
 
@@ -14,32 +35,18 @@ brew update
 # brew tap homebrew/bundle
 brew bundle
 
+echo "Updating preferences using Mackup"
 # Restore dotfiles
 ln -s $(pwd)/dotfiles/.mackup ~/.mackup
 ln -s $(pwd)/dotfiles/.mackup.cfg ~/.mackup.cfg
 mackup restore
 
+echo "Setting preferences"
+
 # Set macOS preferences
 source macos.setup
 source macos.prefs
-
-# Paremeters
-while getopts "n:" opt; do
-case "${opt}" in
-	n) COMPUTER_NAME=${OPTARG};;
-esac
-done
-
-# Set computer name
-# https://ilostmynotes.blogspot.com/2012/03/computername-vs-localhostname-vs.html
-if [[ -z "${COMPUTER_NAME}" ]] ; then
-	scutil --set ComputerName "${COMPUTER_NAME}"
-	scutil --set HostName "${COMPUTER_NAME}"
-	scutil --set LocalHostName "${COMPUTER_NAME}"
-	echo "Computer name set to ${COMPUTER_NAME}"
-else
-	echo "Computer name not set"
-fi
+source macos.nvram
 
 # macOS security 101
 # Definitely check out in more details the README
